1e101f10fa3e2b5e2fc483fe4d21b1d0a238d460
hj <hawking2215@gmail.com>
2024-02-03 08:42:20 +0800

    update

--- 3jsbin_test.go
+++ 3jsbin_test.go
@@ -1,8 +1,13 @@
 package bin
 
 import (
+	"bytes"
+	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
+
+	mst "github.com/flywave/go-mst"
 )
 
 func TestJsLoadFiles(t *testing.T) {
@@ -36,4 +41,63 @@ func TestBinLoadFiles(t *testing.T) {
 	if obj != nil {
 		t.Error("error")
 	}
+}
+
+const absPath = "/home/hj/workspace/GISCore/build/public/Resources/"
+
+func TestToMst(t *testing.T) {
+	dirs := []string{"model/thsk/thsk_sw_xj"} //"anchormodel"
+	for _, d := range dirs {
+		dr := absPath + d
+		fs, _ := readDir(dr, dr, []string{".json"})
+		for _, f := range fs {
+			fpath := dr + f
+			mstPh := strings.Replace(fpath, ".json", ".mst", 1)
+			glbPh := strings.Replace(mstPh, ".mst", ".glb", 1)
+			// if info, err := os.Stat(mstPh); err == nil {
+			// 	t := time.Date(2021, time.November, 16, 12, 40, 0, 0, time.UTC)
+			// 	if info.ModTime().After(t) {
+			// 		continue
+			// 	}
+			// }
+			mh, _ := ThreejsBin2Mst(fpath)
+			for _, nd := range mh.Nodes {
+				nd.ReComputeNormal()
+			}
+			doc := mst.CreateDoc()
+			mst.BuildGltf(doc, mh, false)
+			bt, _ := mst.GetGltfBinary(doc, 8)
+			ioutil.WriteFile(glbPh, bt, os.ModePerm)
+
+			wt := bytes.NewBuffer([]byte{})
+			mst.MeshMarshal(wt, mh)
+			ioutil.WriteFile(mstPh, wt.Bytes(), os.ModePerm)
+		}
+	}
+}
+
+func TestGltf(t *testing.T) {
+	mesh, _ := ThreejsBin2Mst("/home/hj/workspace/GISCore/build/public/Resources/model/public/HHRQQiTiWoLunLiuLiangJi/HHRQQiTiWoLunLiuLiangJi.json")
+	doc := mst.CreateDoc()
+	mst.BuildGltf(doc, mesh, false)
+	bt, _ := mst.GetGltfBinary(doc, 8)
+	ioutil.WriteFile("/home/hj/workspace/GISCore/build/public/Resources/model/public/HHRQQiTiWoLunLiuLiangJi/HHRQQiTiWoLunLiuLiangJi.glb", bt, os.ModePerm)
+}
+
+func TestGltf2(t *testing.T) {
+	mh, _ := ThreejsBin2Mst("/home/hj/workspace/GISCore/build/public/Resources/model/zbrl/ZBRL_BY/ZBRL_BY_1.json")
+
+	doc := mst.CreateDoc()
+	mst.BuildGltf(doc, mh, false)
+	bt, _ := mst.GetGltfBinary(doc, 8)
+	ioutil.WriteFile("/home/hj/workspace/GISCore/build/public/Resources/model/zbrl/ZBRL_BY/ZBRL_BY_1.glb", bt, os.ModePerm)
+}
+
+func TestBin2(t *testing.T) {
+	mh, _ := ThreejsBin2Mst("/home/hj/workspace/GISCore/build/public/Resources/anchormodel/public/psqitong/psqitong.json")
+	doc := mst.CreateDoc()
+	mst.BuildGltf(doc, mh, false)
+	bt, _ := mst.GetGltfBinary(doc, 8)
+	ioutil.WriteFile("/home/hj/workspace/GISCore/build/public/Resources/anchormodel/public/psqitong/psqitong.glb", bt, os.ModePerm)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/flywave/go-3jsbin
 
 go 1.13
+
+require (
+	github.com/flywave/go-mst v0.0.0-20240203004106-5b10fa10c5f4
+	github.com/flywave/go3d v0.0.0-20240109050628-b891a10fca74
+	golang.org/x/image v0.15.0
+)
